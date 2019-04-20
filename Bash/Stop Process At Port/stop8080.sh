60c5affaae0db51f885167fd36c0cd2019decea6
baymac <parichay.barpanda@gmail.com>
2019-04-20 23:02:21 +0530

    added a script to kill process at a specified port

---
+++ Bash/Stop Process At Port/stop8080.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Needs sudo access, kills process at specified port or
+# at port 8080 if no port specified
+
+port=$1
+
+if [ "$port" == "" ]; then
+	port=8080
+fi
+
+echo ${port}
+
+process="$(sudo netstat -nlp | grep :${port})"
+
+id="$(awk '{sub(/\/[a-zA-Z]*/, "", $7); print $7}' <<< ${process})"
+
+sudo kill -9 ${id}
+
+echo "Process running at ${port} is killed forcefully"
